489b26509d932710ab6f252a8b1bbfea79a4f064
y-murata <mura123yasu@gmail.com>
2020-03-31 00:17:24 +0900

    refactor:rename struct

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # cloudevents-cloudpubsub-receiver
 Receive cloud events from cloud pubsub
 
-# Deploy
+## Deploy app to Cloud Functions
 ```sh
 gcloud functions deploy <YOUR FUNCTION NAME> --project <YOUR GCP PROJECT> \
   --entry-point Receiver \
--- receiver.go
+++ receiver.go
@@ -9,27 +9,29 @@ import (
 	cepubsub "github.com/cloudevents/sdk-go/pkg/cloudevents/transport/pubsub"
 )
 
-// published data struct.
-type Example struct {
+// Model is published data struct.
+type Model struct {
 	Sequence int    `json:"id"`
 	Message  string `json:"message"`
 }
 
+// Receiver parse payload to Model
 func Receiver(ctx context.Context, msg *pubsub.Message) error {
 	fmt.Printf("[INFO] message received: %v\n", msg)
 	fmt.Printf("[INFO] message.Attributes: %s\n", msg.Attributes)
 	fmt.Printf("[INFO] message.Data: %s\n", msg.Data)
 
-	// cloudevents pubsubパッケージ内のMessage型に変換する
-	var mess cepubsub.Message
-	mess.Data = msg.Data
-	mess.Attributes = msg.Attributes
-	fmt.Printf("[INFO] CloudEventsVersion: %s\n", mess.CloudEventsVersion())
+	// convert pubsub.Message to cepubsub.Message
+	var cemsg cepubsub.Message
+	cemsg.Data = msg.Data
+	cemsg.Attributes = msg.Attributes
+	fmt.Printf("[INFO] CloudEventsVersion: %s\n", cemsg.CloudEventsVersion())
 
-	data := &Example{}
-	json.Unmarshal([]byte(mess.Data), &data)
-	fmt.Printf("[INFO] Example.Sequence: %d\n", data.Sequence)
-	fmt.Printf("[INFO] Example.Message: %s\n", data.Message)
+	// get data(type Model) from cepubsub.Message
+	data := &Model{}
+	json.Unmarshal([]byte(cemsg.Data), &data)
+	fmt.Printf("[INFO] Model.Sequence: %d\n", data.Sequence)
+	fmt.Printf("[INFO] Model.Message: %s\n", data.Message)
 
 	return nil
 }
